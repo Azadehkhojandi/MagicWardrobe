f434f89dc9ba570a762d4a891d7d14b2625f4441
sayari <sayari.h@gmail.com>
2016-10-15 17:45:18 +1100

    Fixing the header

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ["ngRoute","ui.bootstrap"]);
+var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
