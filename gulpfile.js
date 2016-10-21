21697d0b5e7ede0122faeebbd364a8479e579b47
Azadeh Khojandi <azadehkhojandi@gmail.com>
2016-10-21 12:11:07 +1100

    update gulp file - add open browser and live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,43 @@
-var gulp = require('gulp'),
-  connect = require('gulp-connect');
- 
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); //Runs a local dev server
+var open = require('gulp-open'); //Open a URL in a web browser
+var browserify = require('browserify'); // Bundles JS
+
+var config = {
+	port: 9005,
+	url: 'http://localhost',
+	html: './**/*.html',
+	js: './**/*.js',
+	}
+
+
 gulp.task('connect', function() {
-  connect.server();
+  connect.server(
+  	{
+  		port: config.port,
+		base: config.url,
+		livereload: true
+  	});
 });
- 
- 
-gulp.task('default', ['connect']);
+gulp.task('html', function() {
+	gulp.src(config.html)
+		.pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+	gulp.src(config.js)
+		.pipe(connect.reload());
+}); 
+gulp.task('open', ['connect'], function() {
+	gulp.src('index.html')
+		.pipe(open({ uri: config.url + ':' + config.port + '/'}));
+});
+ gulp.task('watch', function() {
+	gulp.watch(config.html, ['html']);
+	gulp.watch(config.js, ['js' ]);
+	
+});
+
+gulp.task('default', ['open','watch']);
